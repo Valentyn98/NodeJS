810cf7fc77b08971775420c07b1a6d822f50d379
Valentyn98 <valentyn.postolenko98@gmail.com>
2022-02-18 13:28:22 +0200

    hw2 + cw

--- lesson2hw/app.js
+++ lesson2hw/app.js
@@ -98,8 +98,8 @@ const users = [{
 
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'static'))); //сстатик - это стат файлы они будут доступны тем кто скачает продж.
 app.set('view engine','.hbs');//тут будем использовать как двигателем hbs (For template)
 app.engine('.hbs',engine({defaultLayout:false}))//когда будет срабатывать и видеть файлы HBS,используем engine,и указываем настройки
@@ -107,36 +107,49 @@ app.engine('.hbs',engine({defaultLayout:false}))//когда будет сраб
 app.set('views', path.join(__dirname, 'static'));
 
 app.get('/login',(req, res)=>{
-    res.render('login') //когда кто то будет переходить на лоГИн , мы будем давать им страницу Логин
+    res.render('login') ;//когда кто то будет переходить на лоГИн , мы будем давать им страницу Логин
 
 })
+app.get('/signin',((req, res) =>{
+    res.render('signin');
+} ))
+
+app.post('/signin',((req, res) => {
+    let info = users.find(user => user.email === req.body.email && user.password === req.body.password);
+    if (info){
+        res.redirect(`/users/${user.id}`);
+    }else {
+        res.redirect('/error');
+    }
+}))
+
 
 app.get('/users',(req,res)=>{
     const {age,city} = req.query
     let filUsers = [...users]
     if(age){
-        filUsers = users.filter(user => user.age === age)
+        filUsers = users.filter(user => user.age === age);
     }
     else if(city){
-        filUsers = users.filter(user => user.city === city)
+        filUsers = users.filter(user => user.city === city);
     }else if(city && age) {
-        filUsers = users.filter((user => user.city === city && user.age === age))
+        filUsers = users.filter((user => user.city === city && user.age === age));
     }
-    res.render('users',{users: filUsers})
+    res.render('users',{users: filUsers});
+
 })
 
-
 app.post('/login',(req,res)=>{
-    let emailInfo = users.filter(user => user.email === req.body.email)
+    let emailInfo = users.filter(user => user.email === req.body.email);
     // let passwordInfo = users.filter((user=> user.password === req.body.password))
     // if(emailInfo > 0 && passwordInfo > 0){
     //     res.redirect('/signin')
     // }
     if (emailInfo.length > 0 ){
-        res.redirect('/error')
+        res.redirect('/error');
     } else  {
         users.push(req.body)//все что введем юудем пушить в масив юзеров
-        res.redirect('/users')
+        res.redirect('/users');
     }
 })
 // app.get('/signin', (req, res)=>{
@@ -146,17 +159,17 @@ app.post('/login',(req,res)=>{
 //     res.redirect('/signin')
 // } )
 app.get('/users/:userId',(req,res)=>{
-    console.log(req.params)
+    console.log(req.params);
     const {userId} = req.params
-    res.json(users[userId-1])
+    res.json(users[userId-1]);
 })
 
 app.get('/error',(req,res)=>{
-    res.render('error')
+    res.render('error');
 })
 
 app.use((req, res) =>{
-    res.redirect('/error')
+    res.redirect('/error');
 } )
 
  app.listen(5000, ()=>{
